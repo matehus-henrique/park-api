78f88ec89dfcfd9d611f9b3863188a06fc142024
matehus-henrique <mtheusriki@gmail.com>
2024-07-10 14:12:06 -0300

    Requisição via Postman para criar um usuário

--- src/main/java/com/matheus/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/matheus/demo_park_api/service/UsuarioService.java
@@ -1,7 +1,9 @@
 package com.matheus.demo_park_api.service;
 
 
+import com.matheus.demo_park_api.entity.Usuario;
 import com.matheus.demo_park_api.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,4 +14,8 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
