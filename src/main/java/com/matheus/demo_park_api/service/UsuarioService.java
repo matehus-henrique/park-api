9ad7a001f19a8d2c9e72872d7ae2e791ff4fe2a7
matehus-henrique <mtheusriki@gmail.com>
2024-07-12 10:54:00 -0300

    Recurso para listar todos os usuários

--- src/main/java/com/matheus/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/matheus/demo_park_api/service/UsuarioService.java
@@ -3,9 +3,11 @@ package com.matheus.demo_park_api.service;
 
 import com.matheus.demo_park_api.entity.Usuario;
 import com.matheus.demo_park_api.repository.UsuarioRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -19,7 +21,7 @@ public class UsuarioService {
         return usuarioRepository.save(usuario);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Usuario BuscarPorId(Long id) {
         return usuarioRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("Usuario não encontrado")
@@ -31,5 +33,10 @@ public class UsuarioService {
         Usuario user = BuscarPorId(id);
         user.setPassword(password);
         return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> BuscarTodos() {
+        return usuarioRepository.findAll();
     }
 }
