10e0a49dca4a65144a378cb3b40b5dbee92d1ec2
matehus-henrique <mtheusriki@gmail.com>
2024-07-13 14:21:53 -0300

    Validando o objeto UsuarioCreateDto

--- src/main/java/com/matheus/demo_park_api/web/dto/UsuarioCreateDto.java
+++ src/main/java/com/matheus/demo_park_api/web/dto/UsuarioCreateDto.java
@@ -1,10 +1,18 @@
 package com.matheus.demo_park_api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email(message = "formato do E-mail invalido", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$")
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
