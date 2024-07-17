462e7314cbe7937fbbe03a90cd7c91dd933fca76
matehus-henrique <mtheusriki@gmail.com>
2024-07-17 15:56:59 -0300

    Teste ponta a ponta para criar um novo usuário

--- src/test/java/com/matheus/demo_park_api/UsuarioIT.java
+++ src/test/java/com/matheus/demo_park_api/UsuarioIT.java
@@ -1,8 +1,12 @@
 package com.matheus.demo_park_api;
 
 
+import com.matheus.demo_park_api.web.dto.UsuarioCreateDto;
+import com.matheus.demo_park_api.web.dto.UsuarioResponseDto;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -14,4 +18,22 @@ public class UsuarioIT {
 
     @Autowired
     WebTestClient testClient;
+
+    @Test
+    public void createUsuario_ComUsernameEPasswordValidos_RetornarUsuarioCriadoComStatus201(){
+        UsuarioResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("tody@email.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+    }
 }
