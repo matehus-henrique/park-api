6e5e54a66d7b3cbfa48fa7fa6f4c8614f9e0f1fe
matehus-henrique <mtheusriki@gmail.com>
2024-08-15 15:15:02 -0300

    Habilitando o sistema de auditoria

--- src/main/java/com/matheus/demo_park_api/entity/Usuario.java
+++ src/main/java/com/matheus/demo_park_api/entity/Usuario.java
@@ -6,6 +6,11 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -14,6 +19,7 @@ import java.util.Objects;
 @Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
@@ -30,15 +36,19 @@ public class Usuario implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
 
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDate dataCriacao;
 
+    @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDate dataModificacao;
 
+    @CreatedBy
     @Column(name = "criado_por")
     private String criadoPor;
 
+    @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
 
